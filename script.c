2edbda18d6dfa2af403e76bea21b7c0be43d6e1f
Cristhyan D. Marchena <demarchenac@uninorte.edu.co>
2019-03-31 20:02:41 -0500

    Adding more verifications
    No detecta errores #Crying

--- script.c
+++ script.c
@@ -1,13 +1,28 @@
+//hola
+// hola2
 main(void)
-{ int i;
- int j;
- char c;
- char cadena
- float z;
- z=14.9e-8
- z=12.9;
- cadena="Hola";
- scanf("%d",i);
- i=i*2;
- printf("El doble es %d", i);
+{ 
+	int i;
+	int j;
+	char c;
+	char cadena.
+	float z;
+	z=14.9e-8
+	z=12.9;
+	cadena="Hola";
+	scanf("%d",i);
+	if(i%2 == 0){
+		printf;
+	}
+	switch(i){
+		case 1: 
+			printf("1");
+		break;
+		default: 
+			printf("d");
+		break;
+			
+	}
+	i=i*2;
+	printf("El doble es %d", i);
 }
